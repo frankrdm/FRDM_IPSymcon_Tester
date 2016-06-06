2d98aeed0a56bfbf09f935fe5ca958c2eb5ec267
frankrdm <frank@frdm.de>
2016-06-06 16:12:31 +0200

    Update module.php

--- FRDM_IPSymcon_Test/module.php
+++ FRDM_IPSymcon_Test/module.php
@@ -1,5 +1,15 @@
 <?
-class TESTER extends IPSModule
-
-
+    // Klassendefinition
+    class ModulnameXYZ extends IPSModule {
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * ABC_MeineErsteEigeneFunktion($id);
+        *
+        */
+        public function MeineErsteEigeneFunktion() {
+            echo $this->InstanceID;
+        }
+    }
 ?>
